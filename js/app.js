518c09d825b73af81d94f7196e7bb70c29745a3e
Sistemas Universidad Tecnológica de Chetumal <william.sanchez@utchetumal.edu.mx>
2021-10-22 16:39:03 -0500

    sw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var url = window.location.href;
 var swLocation = '/pwa/sw.js';
 
+console.log(url);
 if ( navigator.serviceWorker ) {
     if ( url.includes('localhost') ) {
         swLocation= '/sw.js';
